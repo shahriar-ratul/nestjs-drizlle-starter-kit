ffcea08c9fb5edc170e67b3773f4ce6ab2bca631
Shahriar Ratul <ratul794@gmail.com>
2024-12-30 05:30:18 +0600

    update readme

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,51 @@
-import eslint from '@eslint/js';
-import stylisticTs from '@stylistic/eslint-plugin-ts';
-import tsParser from '@typescript-eslint/parser';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import tseslint from 'typescript-eslint';
-import airbnbConfig from 'eslint-config-airbnb-base';
+import eslint from "@eslint/js";
+import stylisticTs from "@stylistic/eslint-plugin-ts";
+import tsParser from "@typescript-eslint/parser";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
+import airbnbConfig from "eslint-config-airbnb-base";
 export default [
   {
     ignores: [
-      '**/node_modules/*',
-      '**/test-results/*',
-      '**/coverage/*',
-      'eslint.config.mjs',
+      "**/node_modules/*",
+      "**/test-results/*",
+      "**/coverage/*",
+      "eslint.config.mjs",
+      "tsconfig.json",
+      "**/test/*",
+      "**/dist/*",
+      "**/build/*",
     ],
-    files: ['**/*.js', '**/*.ts'],
+    files: ["**/*.js", "**/*.ts", "**/*.tsx", "**/*.jsx"],
     plugins: {
-      '@stylistic/ts': stylisticTs,
-      'simple-import-sort': simpleImportSort,
+      "@stylistic/ts": stylisticTs,
+      "simple-import-sort": simpleImportSort,
       airbnbConfig: airbnbConfig,
     },
 
     languageOptions: {
       parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'script',
+      ecmaVersion: "latest",
+      sourceType: "script",
       parserOptions: {
-        project: './tsconfig.json',
+        project: "./tsconfig.json",
       },
     },
     rules: {
-      '@stylistic/ts/comma-dangle': ['error', 'always-multiline'],
-      '@typescript-eslint/no-floating-promises': ['error'],
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-      'airbnb-base/arrow-body-style': 'off',
+      ...eslintPluginPrettier.configs.recommended.rules,
+      "@stylistic/ts/comma-dangle": ["error", "always-multiline"],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "airbnb-base/arrow-body-style": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: true,
+          trailingComma: "all",
+        },
+      ],
     },
   },
   eslint.configs.recommended,
